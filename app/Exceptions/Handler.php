104d9c1f66a27d303c7bcf64ed5dcd592d9b9573
NurGhulam04 <ghulammusthafa04@gmail.com>
2025-06-04 13:38:25 +0700

    tes no lopgin

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException; // Pastikan ini di-import
 
 class Handler extends ExceptionHandler
 {
@@ -37,5 +38,21 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    /**
+     * Convert an authentication exception into a response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Auth\AuthenticationException  $exception
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        // Ubah ini agar tidak mengarahkan ke rute 'login' yang tidak ada
+        // dan sebaliknya mengarahkan ke rute root ('/') yang sekarang adalah dashboard
+        return $request->expectsJson()
+                    ? response()->json(['message' => $exception->getMessage()], 401)
+                    : redirect()->guest('/'); // Mengarahkan ke rute root
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@ use App\Http\Controllers\ReportsController;
 use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,79 +23,80 @@ use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->middleware('guest');
-Route::post('/', [LoginController::class, 'login'])->name('login');
+// Rute root (/) akan langsung menampilkan dashboard
+// Middleware 'guest' dihapus karena kita ingin langsung masuk
+Route::get('/', [dashboardController::class, 'index'])->name('dashboard_root');
+
+// Jika Anda tidak ingin ada form login sama sekali, komentari atau hapus rute ini
+// Route::post('/', [LoginController::class, 'login'])->name('login');
+
+// Jika Anda masih ingin tombol logout berfungsi, biarkan rute ini,
+// tapi ingat ia tidak akan mengautentikasi pengguna jika tidak ada login
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-// Route::post('/Change-password', [LoginController::class, 'changePassword'])->name('change_password');
 
 
-Route::middleware('auth')->group(function () {
-    Route::get('change-password',[dashboardController::class,'change_password_view'])->name('change_password_view');
-    Route::post('change-password',[dashboardController::class,'change_password'])->name('change_password');
-    Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
+// HAPUS GROUP MIDDLEWARE 'auth' INI!
+// Atau pindahkan rute-rute di dalamnya keluar dari group ini.
+// Semua rute berikut sekarang akan dapat diakses TANPA LOGIN
 
-    // author CRUD
-    Route::get('/authors', [AutherController::class, 'index'])->name('authors');
-    Route::get('/authors/create', [AutherController::class, 'create'])->name('authors.create');
-    Route::get('/authors/edit/{auther}', [AutherController::class, 'edit'])->name('authors.edit');
-    Route::post('/authors/update/{id}', [AutherController::class, 'update'])->name('authors.update');
-    Route::post('/authors/delete/{id}', [AutherController::class, 'destroy'])->name('authors.destroy');
-    Route::post('/authors/create', [AutherController::class, 'store'])->name('authors.store');
+Route::get('change-password',[dashboardController::class,'change_password_view'])->name('change_password_view');
+Route::post('change-password',[dashboardController::class,'change_password'])->name('change_password');
+Route::get('/dashboard', [dashboardController::class, 'index'])->name('dashboard');
 
-    // publisher crud
-    Route::get('/publishers', [PublisherController::class, 'index'])->name('publishers');
-    Route::get('/publisher/create', [PublisherController::class, 'create'])->name('publisher.create');
-    Route::get('/publisher/edit/{publisher}', [PublisherController::class, 'edit'])->name('publisher.edit');
-    Route::post('/publisher/update/{id}', [PublisherController::class, 'update'])->name('publisher.update');
-    Route::post('/publisher/delete/{id}', [PublisherController::class, 'destroy'])->name('publisher.destroy');
-    Route::post('/publisher/create', [PublisherController::class, 'store'])->name('publisher.store');
+// author CRUD
+Route::get('/authors', [AutherController::class, 'index'])->name('authors');
+Route::get('/authors/create', [AutherController::class, 'create'])->name('authors.create');
+Route::get('/authors/edit/{auther}', [AutherController::class, 'edit'])->name('authors.edit');
+Route::post('/authors/update/{id}', [AutherController::class, 'update'])->name('authors.update');
+Route::post('/authors/delete/{id}', [AutherController::class, 'destroy'])->name('authors.destroy');
+Route::post('/authors/create', [AutherController::class, 'store'])->name('authors.store');
 
-    // Category CRUD
-    Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
-    Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
-    Route::get('/category/edit/{category}', [CategoryController::class, 'edit'])->name('category.edit');
-    Route::post('/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
-    Route::post('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
-    Route::post('/category/create', [CategoryController::class, 'store'])->name('category.store');
+// publisher crud
+Route::get('/publishers', [PublisherController::class, 'index'])->name('publishers');
+Route::get('/publisher/create', [PublisherController::class, 'create'])->name('publisher.create');
+Route::get('/publisher/edit/{publisher}', [PublisherController::class, 'edit'])->name('publisher.edit');
+Route::post('/publisher/update/{id}', [PublisherController::class, 'update'])->name('publisher.update');
+Route::post('/publisher/delete/{id}', [PublisherController::class, 'destroy'])->name('publisher.destroy');
+Route::post('/publisher/create', [PublisherController::class, 'store'])->name('publisher.store');
 
+// Category CRUD
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories');
+Route::get('/category/create', [CategoryController::class, 'create'])->name('category.create');
+Route::get('/category/edit/{category}', [CategoryController::class, 'edit'])->name('category.edit');
+Route::post('/category/update/{id}', [CategoryController::class, 'update'])->name('category.update');
+Route::post('/category/delete/{id}', [CategoryController::class, 'destroy'])->name('category.destroy');
+Route::post('/category/create', [CategoryController::class, 'store'])->name('category.store');
 
+// books CRUD
+Route::get('/books', [BookController::class, 'index'])->name('books');
+Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
+Route::get('/book/edit/{book}', [BookController::class, 'edit'])->name('book.edit');
+Route::post('/book/update/{id}', [BookController::class, 'update'])->name('book.update');
+Route::post('/book/delete/{id}', [BookController::class, 'destroy'])->name('book.destroy');
+Route::post('/book/create', [BookController::class, 'store'])->name('book.store');
 
+// students CRUD
+Route::get('/students', [StudentController::class, 'index'])->name('students');
+Route::get('/student/create', [StudentController::class, 'create'])->name('student.create');
+Route::get('/student/edit/{student}', [StudentController::class, 'edit'])->name('student.edit');
+Route::post('/student/update/{id}', [StudentController::class, 'update'])->name('student.update');
+Route::post('/student/delete/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
+Route::post('/student/create', [StudentController::class, 'store'])->name('student.store');
+Route::get('/student/show/{id}', [StudentController::class, 'show'])->name('student.show');
 
-    // books CRUD
-    Route::get('/books', [BookController::class, 'index'])->name('books');
-    Route::get('/book/create', [BookController::class, 'create'])->name('book.create');
-    Route::get('/book/edit/{book}', [BookController::class, 'edit'])->name('book.edit');
-    Route::post('/book/update/{id}', [BookController::class, 'update'])->name('book.update');
-    Route::post('/book/delete/{id}', [BookController::class, 'destroy'])->name('book.destroy');
-    Route::post('/book/create', [BookController::class, 'store'])->name('book.store');
+Route::get('/book_issue', [BookIssueController::class, 'index'])->name('book_issued');
+Route::get('/book-issue/create', [BookIssueController::class, 'create'])->name('book_issue.create');
+Route::get('/book-issue/edit/{id}', [BookIssueController::class, 'edit'])->name('book_issue.edit');
+Route::post('/book-issue/update/{id}', [BookIssueController::class, 'update'])->name('book_issue.update');
+Route::post('/book-issue/delete/{id}', [BookIssueController::class, 'destroy'])->name('book_issue.destroy');
+Route::post('/book-issue/create', [BookIssueController::class, 'store'])->name('book_issue.store');
 
-    // students CRUD
-    Route::get('/students', [StudentController::class, 'index'])->name('students');
-    Route::get('/student/create', [StudentController::class, 'create'])->name('student.create');
-    Route::get('/student/edit/{student}', [StudentController::class, 'edit'])->name('student.edit');
-    Route::post('/student/update/{id}', [StudentController::class, 'update'])->name('student.update');
-    Route::post('/student/delete/{id}', [StudentController::class, 'destroy'])->name('student.destroy');
-    Route::post('/student/create', [StudentController::class, 'store'])->name('student.store');
-    Route::get('/student/show/{id}', [StudentController::class, 'show'])->name('student.show');
+Route::get('/reports', [ReportsController::class, 'index'])->name('reports');
+Route::get('/reports/Date-Wise', [ReportsController::class, 'date_wise'])->name('reports.date_wise');
+Route::post('/reports/Date-Wise', [ReportsController::class, 'generate_date_wise_report'])->name('reports.date_wise_generate');
+Route::get('/reports/monthly-Wise', [ReportsController::class, 'month_wise'])->name('reports.month_wise');
+Route::post('/reports/monthly-Wise', [ReportsController::class, 'generate_month_wise_report'])->name('reports.month_wise_generate');
+Route::get('/reports/not-returned', [ReportsController::class, 'not_returned'])->name('reports.not_returned');
 
-
-
-    Route::get('/book_issue', [BookIssueController::class, 'index'])->name('book_issued');
-    Route::get('/book-issue/create', [BookIssueController::class, 'create'])->name('book_issue.create');
-    Route::get('/book-issue/edit/{id}', [BookIssueController::class, 'edit'])->name('book_issue.edit');
-    Route::post('/book-issue/update/{id}', [BookIssueController::class, 'update'])->name('book_issue.update');
-    Route::post('/book-issue/delete/{id}', [BookIssueController::class, 'destroy'])->name('book_issue.destroy');
-    Route::post('/book-issue/create', [BookIssueController::class, 'store'])->name('book_issue.store');
-
-    Route::get('/reports', [ReportsController::class, 'index'])->name('reports');
-    Route::get('/reports/Date-Wise', [ReportsController::class, 'date_wise'])->name('reports.date_wise');
-    Route::post('/reports/Date-Wise', [ReportsController::class, 'generate_date_wise_report'])->name('reports.date_wise_generate');
-    Route::get('/reports/monthly-Wise', [ReportsController::class, 'month_wise'])->name('reports.month_wise');
-    Route::post('/reports/monthly-Wise', [ReportsController::class, 'generate_month_wise_report'])->name('reports.month_wise_generate');
-    Route::get('/reports/not-returned', [ReportsController::class, 'not_returned'])->name('reports.not_returned');
-
-    Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
-    Route::post('/settings', [SettingsController::class, 'update'])->name('settings');
-});
+Route::get('/settings', [SettingsController::class, 'index'])->name('settings');
+Route::post('/settings', [SettingsController::class, 'update'])->name('settings');
